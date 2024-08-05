d3d91b94ec406871d9877e27281dd5779863d81a
Mathie <math039@outlook.fr>
2024-08-05 14:56:46 +0200

    Section1

--- vite-project/components/Section1.jsx
+++ vite-project/components/Section1.jsx
@@ -11,19 +11,25 @@ export const Section1 = () => {
       <div className="Flex">
         <Clients
           img={ImgEmily}
-          Prenom={"Emily R."}
-          role={"Marketing Director"}
           Texte={
             "We put our trust in Sunnyside and they delivered,making sure our needs were met and deadlines were always hit."
           }
+          Prenom={"Emily R."}
+          role={"Marketing Director"}
         />
         <Clients
           img={ImgThomas}
+          Texte={
+            "Sunnyside's enthusian coupled with their keen interest in our brand's succes made it a satisfying and enjoyable experience"
+          }
           Prenom={"Thomas S."}
           role={"Chief Operating  Officer"}
         />
         <Clients
           img={ImgJennie}
+          Texte={
+            "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended !"
+          }
           Prenom={"Jennie F."}
           role={"Buisiness Owner"}
         />
