6cab515981833e536b93cd0c433ed233a30fccc4
Mathie <math039@outlook.fr>
2024-08-08 21:54:03 +0200

    deploy2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,11 +9,6 @@ import { Main } from "../components/Main";
 import Section1 from "../components/Section1";
 import Section2 from "../components/Section2";
 import Footer1 from "../components/TextComponant/Footer1";
-// Desktpp img
-
-// mobile img
-
-// AUTRES IMG
 
 import logo from "../Images/imagesnonresponsive/logo.svg";
 import FlecheDuBas from "../Images/imagesnonresponsive/icon-arrow-down.svg";
